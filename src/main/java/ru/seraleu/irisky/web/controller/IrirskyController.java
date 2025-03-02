0fa4d40434f769ea7572847c9da6d42f414eaede
admin <sapozhnikoff1996@gmail.com>
2025-03-02 13:21:13 +0300

    fin

--- src/main/java/ru/seraleu/irisky/web/controller/IrirskyController.java
+++ src/main/java/ru/seraleu/irisky/web/controller/IrirskyController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.seraleu.irisky.service.DataService;
 import ru.seraleu.irisky.service.EpkClientService;
 import ru.seraleu.irisky.service.MainService;
+import ru.seraleu.irisky.utils.IriskyUtils;
 import ru.seraleu.irisky.web.dto.pprb.GenuuidDTO;
 import ru.seraleu.irisky.web.dto.pprb.identifier.CreditHistIdentifier;
 
@@ -48,7 +49,7 @@ public class IrirskyController {
 
     @PostMapping(value = "/save-credit-hist-processing-finish", consumes = "application/json", produces = "application/json")
     public ResponseEntity<String> saveCreditHistProcessingAgentEntityFinishCalculating(@RequestBody String response) {
-        log.info("Saving response unvalidated into credit_hist_processing_agent table. response: " + '\n' + response);
+        log.info("Saving unvalidated response into credit_hist_processing_agent table. response: " + '\n' + response);
         try {
             mainService.saveCreditHistProcessingAgentEntityFinishCalculating(response);
             return ResponseEntity.status(HttpStatus.OK).build();
@@ -84,10 +85,9 @@ public class IrirskyController {
 
     @PostMapping(value = "/get-report", consumes = "application/json", produces = "application/json")
     public ResponseEntity<String> getReport(@RequestBody GenuuidDTO uuid){
-        log.info("Запросили отчет для " + uuid );
-
+        log.info("Credit report was requested for uuid " + uuid );
         return ResponseEntity.status(HttpStatus.OK)
-                .body(dataService.getCreditHistProcessingAgentEntityByGenuuid(uuid.getGenuuid()).getResult());
+                .body(IriskyUtils.formatReport(dataService.getCreditHistProcessingAgentEntityByGenuuid(uuid.getGenuuid()).getResult()));
 
     }
 
--- src/main/resources/db/changelog/h2/irisky/tables/processing_result_validation_agent.sql
+++ src/main/resources/db/changelog/h2/irisky/tables/processing_result_validation_agent.sql
@@ -1,16 +1,16 @@
---liquibase formatted sql
-
---changeset ser_aleu:a3161fc4-89a8-4c14-b6bc-fb86246f3b42 runOnChange:true failOnError:true
---comment: CREATE TABLE processing_result_validation_agent
---preconditions onFail:MARK_RAN onError:MARK_RAN
---preconditions-sql-check expectedResult: 0 SELECT COUNT(*) AS count FROM information_schema.tables WHERE table_schema = 'i_risky' table_name = 'processing_result_validation_agent'
-CREATE TABLE i_risky.processing_result_validation_agent (
-    id INT NOT NULL auto_increment,
-    credit_hist_processing_agent_uuid VARCHAR NOT NULL,
-    status VARCHAR(100),
-    added_at TIMESTAMP,
-    result VARCHAR,
-    error_details VARCHAR,
-    PRIMARY KEY (id)
-)
---rollback DROP TABLE i_risky.processing_result_validation_agent
+----liquibase formatted sql
+--
+----changeset ser_aleu:a3161fc4-89a8-4c14-b6bc-fb86246f3b42 runOnChange:true failOnError:true
+----comment: CREATE TABLE processing_result_validation_agent
+----preconditions onFail:MARK_RAN onError:MARK_RAN
+----preconditions-sql-check expectedResult: 0 SELECT COUNT(*) AS count FROM information_schema.tables WHERE table_schema = 'i_risky' table_name = 'processing_result_validation_agent'
+--CREATE TABLE i_risky.processing_result_validation_agent (
+--    id INT NOT NULL auto_increment,
+--    credit_hist_processing_agent_uuid VARCHAR NOT NULL,
+--    status VARCHAR(100),
+--    added_at TIMESTAMP,
+--    result VARCHAR,
+--    error_details VARCHAR,
+--    PRIMARY KEY (id)
+--)
+----rollback DROP TABLE i_risky.processing_result_validation_agent
